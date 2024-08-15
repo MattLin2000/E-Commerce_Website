2ccfcfee0f5012e4bef3140b7c77129d1d443424
“DaiCJz” <105777211+DaiCJz@users.noreply.github.com>
2024-08-15 16:56:26 +0800

    add ignore

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,18 +1,12 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-<<<<<<< HEAD
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class })
-=======
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
->>>>>>> 95662592c5744e560d5f5c0be94892faa69bd0e1
 public class DemoApplication {
 
 	public static void main(String[] args) {
